6fcbf804d4cd7c3b8311c0086fb747ad9f0d397a
kurumadaisuke <kuruma0909@gmail.com>
2024-11-18 16:12:35 +0900

    static_content/s3.tf

---
+++ static_content/s3.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "static_content_bucket" {
+  bucket = "static-content-test"
+
+  tags = {
+    Name        = "test-bucket"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "static_content_encryption" {
+  bucket = aws_s3_bucket.static_content_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }  
+}
+
+resource "aws_s3_bucket_website_configuration" "static_content_website" {
+  bucket = aws_s3_bucket.static_content_bucket.bucket
+
+  index_document {
+    suffix = "sorry.html"
+  }
+}
